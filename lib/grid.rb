e3e549b826ea9206df6273ce3ce3cb8b968b9ae1
Padraic O'Neill <pconeill@gmail.com>
2017-04-03 15:28:44 -0600

    Grid#parse_coordinates, #create_grid_index

--- lib/grid.rb
+++ lib/grid.rb
@@ -21,4 +21,25 @@ class Grid
       end
     end
   end
+
+  def parse_coordinates(coordinates)
+    first_cell = coordinates[0..1].chars
+    second_cell = coordinates[-2..-1].chars
+    #example: ["A", "1"] ["B", "1"]
+  end
+
+  def create_grid_index
+    rows = ("A".."D").to_a
+    #ex: ["A", "B", "C", "D"]
+    columns = (1..4).to_a
+    #ex: [1, 2, 3, 4]
+  end
+
+
+
+
+
+
+
+
 end
