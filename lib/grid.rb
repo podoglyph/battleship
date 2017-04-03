93f161cd52d393de3411a77d3a7fd6e70bdfb594
Padraic O'Neill <pconeill@gmail.com>
2017-04-03 15:54:16 -0600

    Grid#create_grid_index normalizes user_coordinates

--- lib/grid.rb
+++ lib/grid.rb
@@ -25,21 +25,17 @@ class Grid
   def parse_coordinates(coordinates)
     first_cell = coordinates[0..1].chars
     second_cell = coordinates[-2..-1].chars
-    #example: ["A", "1"] ["B", "1"]
+    #ex: ["A", "1"] ["B", "1"]
   end
 
   def create_grid_index
     rows = ("A".."D").to_a
-    #ex: ["A", "B", "C", "D"]
+    j = - 1
+    normalized_rows = rows.map {|x| x = j += 1 }
+    #ex: ["A", "B", "C", "D"] becomes [0, 1, 2, 3]
     columns = (1..4).to_a
-    #ex: [1, 2, 3, 4]
+    normalized_columns = columns.map {|x| x -= 1}
+    #ex: [1, 2, 3, 4] becomes [0, 1, 2, 3]
   end
-
-
-
-
-
-
-
 
 end
