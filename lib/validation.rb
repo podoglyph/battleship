98332498b374a1831a23baa676f0e94af36f3002
Padraic O'Neill <pconeill@gmail.com>
2017-04-04 11:41:37 -0600

    refactor Validation to pass variables around more efficiently

--- lib/validation.rb
+++ lib/validation.rb
@@ -1,31 +1,32 @@
 require 'pry'
 require 'pry-state'
 
-module Validation
+class Validation
 
   def self.validate_coordinate_positions?(all_cells, v_range)
     true if all_cells.all?{ |x| v_range.include?(x)}
   end
 
+  private
   def self.parse_coordinates(coordinates, ship_size)
     if ship_size == 2
       first_cell, second_cell = [], []
       first_cell = coordinates[0..1].chars #["A", "1"]
       second_cell = coordinates[-2..-1].chars #["B", "1"]
-      @all_cells = first_cell.concat(second_cell)
+      all_cells = first_cell.concat(second_cell)
     end
     if ship_size == 3
       first_cell, second_cell, third_cell = [], [], []
       first_cell = coordinates[0..1].chars #["A", "1"]
       second_cell = coordinates[3..4].chars #["B", "1"]
       third_cell = coordinates[-2..-1].chars
-      @all_cells = first_cell.concat(second_cell).concat(third_cell)
+      all_cells = first_cell.concat(second_cell).concat(third_cell)
     end
-    build_validation_range
+    build_validation_range(all_cells)
   end
 
-  def self.build_validation_range
-    @v_range = ("A".."D").to_a.concat(("1".."4").to_a)
-    validate_coordinate_positions?(@all_cells, @v_range)
+  def self.build_validation_range(all_cells)
+    v_range = ("A".."D").to_a.concat(("1".."4").to_a)
+    validate_coordinate_positions?(all_cells, v_range)
   end
 end
