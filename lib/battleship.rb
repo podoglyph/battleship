f03e38ecaca3ca749f6f8415b4db2ebe74c8c5c0
Padraic O'Neill <pconeill@gmail.com>
2017-04-06 03:24:41 -0600

    mapping ships to cells

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -3,7 +3,7 @@ require 'pry'
 require 'pry-state'
 
 class Battleship
-  attr_reader :player, :computer, :deploy_location, :deploy_location
+  attr_reader :player, :computer, :deploy_location2, :deploy_location3, :coordinates
 
   def initialize
     @player = Player.new
@@ -35,18 +35,34 @@ class Battleship
   end
 
   def prepare_computer_ships
-    @deploy_location = computer.two_unit_ship.location
-    @deploy_location = computer.three_unit_ship.location
-    find_comp_destination
+    @deploy_location2 = computer.two_unit_ship.location
+    @deploy_location3 = computer.three_unit_ship.location
+    locate_destination #[[2, 0], [3, 0], [1, 0]]
   end
 
-  def find_comp_destination
+  def locate_destination
+    row1 = @grid.game_grid[0]
 
+    row1.each do |i|
+      @coordinates = []
+      @coordinates << i.x << i.y
+      print coordinates
+    end
+
+    row2 = @grid.game_grid[1]
+
+    row3 = @grid.game_grid[2]
+
+    row4 = @grid.game_grid[3]
+
+    binding.pry
   end
-
 
   def run
     welcome_prompt
   end
 
 end
+b = Battleship.new
+binding.pry
+""
--- lib/cell.rb
+++ lib/cell.rb
@@ -4,11 +4,12 @@ require 'pry-state'
 
 class Cell
   attr_reader :x, :y
-  attr_accessor :has_ship, :fired_upon
+  attr_accessor :has_ship, :fired_upon, :captain
 
   def initialize(x, y)
     @has_ship = false
     @fired_upon = ""
+    @captain = nil
     @x = x
     @y = y
   end
