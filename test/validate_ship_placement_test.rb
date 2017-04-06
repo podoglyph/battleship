541eb985dc6a7e60ffe3ac847ec95418937b7993
Padraic O'Neill <pconeill@gmail.com>
2017-04-05 22:15:36 -0600

    fix validate_ship_placement test

--- test/validate_ship_placement_test.rb
+++ test/validate_ship_placement_test.rb
@@ -5,20 +5,20 @@ require 'minitest/pride'
 class ValidateShipPlacementTest < Minitest::Test
 
   def test_class_exists
-    v = ValidateShipPlacement.new
+    v = ValidateShipPlacement.new("A1 B1", 2)
 
     assert_equal ValidateShipPlacement, v.class
   end
 
   def test_can_receive_return_value_from_validate_on_map
-    v = ValidateShipPlacement.new
+    v = ValidateShipPlacement.new("A1 B1", 2)
     vm = v.v_map.parse_coordinates
 
     assert_equal true, vm
   end
 
   def test_isolated_validation_gets_values
-    v = ValidateShipPlacement.new
+    v = ValidateShipPlacement.new("A1 B1", 2)
     v1 = v.three_ship_coordinates
 
     assert_equal true, v1
