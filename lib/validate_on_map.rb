e88cf8db1a4d072809da3d9acaf93e9efb3b1f47
Padraic O'Neill <pconeill@gmail.com>
2017-04-23 20:21:27 -0600

    refactor ValidateOnMap module

--- lib/validate_on_map.rb
+++ lib/validate_on_map.rb
@@ -1,44 +1,19 @@
 require './lib/messages.rb'
 require 'pry'
 
-class ValidateOnMap
+module ValidateOnMap
+  include Messages
 
-  def initialize(coordinates, ship_size)
-    @coordinates = coordinates
-    @ship_size = ship_size
+  def verify_coordinates?(input)
+    parse(input).all? { |x| grid_range.include?(x)}
   end
 
-  def validate_coordinate_positions?
-    return true if @all_cells.all?{ |x| @v_range.include?(x)}
-    puts Messages.place_ship_on_map
-    false
+  def parse(input)
+    input.chars.delete_if {|x| x == " "}
   end
 
-  def parse_coordinates(indices = false)
-    if @ship_size == 2
-      first_cell, second_cell = [], []
-      first_cell = @coordinates[0..1].chars #["A", "1"]
-      second_cell = @coordinates[-2..-1].chars #["B", "1"]
-      @all_cells = first_cell.concat(second_cell)
-      if indices == true
-        return @all_cells.each_slice(2).to_a
-      end
-    end
-    if @ship_size == 3
-      first_cell, second_cell, third_cell = [], [], []
-      first_cell = coordinates[0..1].chars #["A", "1"]
-      second_cell = coordinates[3..4].chars #["B", "1"]
-      third_cell = coordinates[-2..-1].chars
-      @all_cells = first_cell.concat(second_cell).concat(third_cell)
-      if indices == true
-        return @all_cells.each_slice(2).to_a
-      end
-    end
-    build_validation_range
+  def grid_range
+    ("A".."D").to_a.concat(("1".."4").to_a)
   end
 
-  def build_validation_range
-    @v_range = ("A".."D").to_a.concat(("1".."4").to_a)
-    validate_coordinate_positions?
-  end
 end
--- test/validate_on_map_test.rb
+++ test/validate_on_map_test.rb
@@ -3,32 +3,6 @@ require './lib/validate_on_map.rb'
 
 class ValidateOnMapTest < Minitest::Test
 
-  def test_class_exists
-    v = ValidateOnMap.new("A1 B1", 2)
-
-    assert_instance_of ValidateOnMap, v
-  end
-
-  def test_first_step_validate_coordinates_on_map
-    coordinates = "A1 B1"
-    ship_size = 2
-
-    v = ValidateOnMap.new(coordinates, ship_size)
-
-    v.parse_coordinates(indices = false)
-
-    validation = v.validate_coordinate_positions?
-
-    assert_equal true, validation
-
-    coordinates2 = "A7 B1"
-    v1 = ValidateOnMap.new(coordinates2, ship_size)
-
-    v1.parse_coordinates(indices = false)
-
-    validation1 = v1.validate_coordinate_positions?
-
-    assert_equal false, validation1
-  end
+  
 
 end
