a1db4ad43b7ba1b6adef1a4c79f057097a3828c6
Padraic O'Neill <pconeill@gmail.com [core]>
2017-04-30 14:28:47 -0600

    fix CellTest

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -4,20 +4,20 @@ require './lib/cell.rb'
 class CellTest < Minitest::Test
 
   def test_class_exists
-    c = Cell.new(0, 1)
+    c = Cell.new('A1')
 
     assert_equal Cell, c.class
   end
 
   def test_attributes_are_readable
-    c = Cell.new(0, 1)
+    c = Cell.new('A1')
 
     assert_equal false, c.has_ship
     assert_equal "", c.fired_upon
   end
 
   def test_cell_can_have_ship
-    c = Cell.new(0, 1)
+    c = Cell.new('A1')
 
     assert_equal false, c.has_ship
 
@@ -27,17 +27,10 @@ class CellTest < Minitest::Test
   end
 
   def test_cell_can_take_a_hit
-    c = Cell.new(0, 1)
+    c = Cell.new('A1')
     c.fired_upon = "h"
 
     assert_equal "h", c.fired_upon
-  end
-
-  def test_can_pass_arguments_into_cell_on_creation
-    c = Cell.new(0, 1)
-
-    assert_equal 0, c.x
-    assert_equal 1, c.y
   end
 
 end
